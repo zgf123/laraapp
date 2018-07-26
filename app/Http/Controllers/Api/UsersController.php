ab0dbb2070993de3f3ea4fe2de730e0b8261816e
zgf123 <277845885@qq.com>
2018-07-26 15:49:57 +0000

    transformer

--- app/Http/Controllers/Api/UsersController.php
+++ app/Http/Controllers/Api/UsersController.php
@@ -5,6 +5,8 @@ namespace App\Http\Controllers\Api;
 use Illuminate\Http\Request;
 use App\Models\User;
 use App\Http\Requests\Api\UserRequest;
+use App\Transformers\UserTransformer;
+use App\Models\Image;
 
 class UsersController extends Controller
 {
@@ -27,6 +29,31 @@ class UsersController extends Controller
 
         \Cache::forget($request->verification_key);
 
-        return $this->response->created();
+        return $this->response->item($user, new UserTransformer())
+        ->setMeta([
+            'access_token' => \Auth::guard('api')->fromUser($user),
+            'token_type' => 'Bearer',
+            'expires_in' => \Auth::guard('api')->factory()->getTTL() * 60
+        ])
+        ->setStatusCode(201);
+    }
+
+    public function update(UserRequest $request){
+        $user = $this->user();
+
+        $attributes = $request->only(['name', 'email', 'introduction']);
+
+        if ($request->avatar_image_id) {
+            $image = Image::find($request->avatar_image_id);
+
+            $attributes['avatar'] = $image->path;
+        }
+        $user->update($attributes);
+
+        return $this->response->item($user, new UserTransformer());
+    }
+
+    public function me(){
+        return $this->response->item($this->user(), new UserTransformer());
     }
 }
